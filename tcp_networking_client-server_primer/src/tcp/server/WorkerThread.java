2f099015c02d79514bd4a7ce5944b4e6a58748c7
BojanChe04 <153726412+BojanChe04@users.noreply.github.com>
2025-05-13 23:13:16 +0200

    Add files via upload

---
+++ tcp_networking_client-server_primer/src/tcp/server/WorkerThread.java
@@ -0,0 +1,65 @@
+package tcp.server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class WorkerThread extends Thread {
+
+    private Socket socket = null;
+
+    public WorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+    //socket na nivo na os e obicen file
+        //ako sakame da citame i zapisvime podatoci mojme da koristime strimovi
+        //zato gi kreirame ovie, za da ni ja ovozmozet komunikacijata
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+
+        try{
+            System.out.printf("Connected:%s:%d\n",socket.getInetAddress(),socket.getPort());
+
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream());
+
+            //obicna komunikacija klient-server kje naprajme
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+
+                //pecatime so ni pratil klientot
+                System.out.printf("New message from%s:%d:%s",socket.getInetAddress(),socket.getPort(),line);
+                writer.write(line);//eho prajme
+                writer.flush();//morat sekogas na kraj da si stavame za da se transmitova porakat
+            }
+            socket.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(reader != null){
+                try {
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(writer != null){
+                writer.close();
+            }
+        }
+    }
+}
